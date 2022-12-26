8e58fd24431dc7073deddf5ba82fc00d9e59ef58
cctutorial7thsem <121408732+cctutorial7thsem@users.noreply.github.com>
2022-12-26 05:30:52 +0530

    Changed port to 80

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get('/', function(req, res) {
 
 
 
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
+app.listen(80, function () {
+  console.log('App listening on port 80');
 });
 
